c332828ee4bb9f6eeed95dfb5ba13479ded5302d
NaCl-NOB <74619382+NaCl-NOB@users.noreply.github.com>
2020-12-15 21:21:05 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -24,6 +24,7 @@ async def ping(ctx):
 @bot.command()   
 async def neko(ctx):
     await ctx.send(random.choice(('ﾔｰ','ﾔｰ','ﾔｰ','ﾔｰ','ﾔｰ','ﾔｰ','ﾔｰﾃﾞ','ﾔｰﾃﾞ','ﾔｰﾃﾞ','ﾔｰﾃﾞ','ﾔｰﾃﾞ','ええ加減にせぇや','何がおもろいん?','......うちも暇やないんやけど')))
+@bot.command() 
 async def AO-1(ctx):
     await ctx.send(':blue_circle:')
     

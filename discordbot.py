fd2c358dc867d719f69c69d935836c318649cd35
NaCl-NOB <74619382+NaCl-NOB@users.noreply.github.com>
2020-12-16 19:28:26 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,11 +1,11 @@
-from discord.ext import commands
+import discord
 import traceback
 import random
 import io
 import asyncio
 import os
-import discord
-import numpy as np 
+import numpy as np
+from discord.ext import commands
 
 BOT_PREFIX = ('!')
 #bot = commands.Bot(command_prefix= BOT_PREFIX)
@@ -39,9 +39,10 @@ async def neko(ctx):
 
 
 #以降はオゾン層さんからの頂き物の改造
+
 @client.command()
 async def hoge():
-    await message.channel.send("fuga")
+    await client.channel.send("fuga")
 
 
 # ユーザーの発言に対する反応を書く
@@ -57,67 +58,67 @@ async def on_message(message):
       return
 
   # 文の最初が一致した場合にメッセージを返す
-      if message.content.startswith("ステルス"):
-        await message.channel.send("「ス」めー。")
-      if message.content.startswith("平沢"):
-        await message.channel.send("東京の平沢です。")
-      if message.content.startswith("回"):
-        await message.channel.send("今日もタービンは回っている")
-      if message.content.startswith("おはよう"):
-        await message.channel.send("ご機嫌よう")
-      if message.content.startswith("何だかんだ"):
-        await message.channel.send("次は、神田です")
-      if message.content.startswith("ｳﾙ"):
-        await message.channel.send("うるさい。")
-      if message.content.startswith("うるさい"):
-        await message.channel.send("やかましい。.wav")
-        
+    if message.content.startswith("ステルス"):
+      await message.channel.send("「ス」めー。")
+    if message.content.startswith("平沢"):
+      await message.channel.send("東京の平沢です。")
+    if message.content.startswith("回"):
+      await message.channel.send("今日もタービンは回っている")
+    if message.content.startswith("おはよう"):
+      await message.channel.send("ご機嫌よう")
+    if message.content.startswith("何だかんだ"):
+      await message.channel.send("次は、神田です")
+    if message.content.startswith("ｳﾙ"):
+      await message.channel.send("うるさい。")
+    if message.content.startswith("うるさい"):
+      await message.channel.send("やかましい。.wav")
+      
+    # 後でメンションにする
+      """
+    if message.content.startswith("asso"):
+      await message.channel.send(":pleading_face:")
+    if message.content.startswith("コンギョ"):
+      await message.channel.send(":flag_kp: :rocket: :boom:")
+    if message.content.startswith("かに"):
+      await message.channel.send(":crab:")
+    if message.content.startswith("塩"):
+      await message.channel.send(":salt:")
+    if message.content.startswith("ﾊﾞｹ"):
+      await message.channel.send(":japanese_ogre:")
+      """
+
+    if message.content.startswith("java"):
+      await message.channel.send(":coffee:")
+  
+
+# 文中の内容が一致した場合にメッセージを返す
+    if True in [i in message.content for i in ["死ね", "氏ね", "ﾀﾋね", "馬鹿にし", "馬鹿じゃね", "下がってろ"]]:
+      async with message.channel.typing():
+        await asyncio.sleep(0.5)
+        await message.channel.send("なんや喧嘩か？")
+
+
+    if True in [i in message.content for i in ["出禁", "dekin", ":dekin:", "出来ん", "Dekin", "できん", "デキン"]]:
+      async with message.channel.typing():
+        await asyncio.sleep(0.5)
+        await message.channel.send("||~~出禁に出来ん~~||")
+
       # 後でメンションにする
-        """
-      if message.content.startswith("asso"):
-        await message.channel.send(":pleading_face:")
-      if message.content.startswith("コンギョ"):
-        await message.channel.send(":flag_kp: :rocket: :boom:")
-      if message.content.startswith("かに"):
-        await message.channel.send(":crab:")
-      if message.content.startswith("塩"):
-        await message.channel.send(":salt:")
-      if message.content.startswith("ﾊﾞｹ"):
-        await message.channel.send(":japanese_ogre:")
-        """
+      """
+      if message.guild.id == 494052154290601985:
+        await message.add_reaction("<:dekin:556403106045493258>")
+      else:
+        await message.channel.send(":u7981:")
+      """
 
-      if message.content.startswith("java"):
-        await message.channel.send(":coffee:")
-    
-
-  # 文中の内容が一致した場合にメッセージを返す
-      if True in [i in message.content for i in ["死ね", "氏ね", "ﾀﾋね", "馬鹿にし", "馬鹿じゃね", "下がってろ"]]:
+    # 文中の内容が一致した場合に、数種類のメッセージのうち1つをランダムに選んで返す
+    elif True in [i in message.content for i in ["ｱｶﾈﾁｬﾝ", "琴葉", "琴葉茜", "あかねちゃん", "茜ちゃん"]]:
+      async with message.channel.typing():
+        await asyncio.sleep(0.5)
+        say_list = ["呼んだ？", "やぁ。", "やかましい。.wav", "せやな", "やあやあ諸君", "ｱｶﾈﾁｬﾝやで", "なんや？", "みんな大好きｱｶﾈﾁｬﾝやで", "zzz", "......"]
         async with message.channel.typing():
-          await asyncio.sleep(0.5)
-          await message.channel.send("なんや喧嘩か？")
-
-
-      if True in [i in message.content for i in ["出禁", "dekin", ":dekin:", "出来ん", "Dekin", "できん", "デキン"]]:
-        async with message.channel.typing():
-          await asyncio.sleep(0.5)
-          await message.channel.send("||~~出禁に出来ん~~||")
-
-        # 後でメンションにする
-        """
-        if message.guild.id == 494052154290601985:
-          await message.add_reaction("<:dekin:556403106045493258>")
-        else:
-          await message.channel.send(":u7981:")
-        """
-
-      # 文中の内容が一致した場合に、数種類のメッセージのうち1つをランダムに選んで返す
-      elif True in [i in message.content for i in ["ｱｶﾈﾁｬﾝ", "琴葉", "琴葉茜", "あかねちゃん", "茜ちゃん"]]:
-        async with message.channel.typing():
-          await asyncio.sleep(0.5)
-          say_list = ["呼んだ？", "やぁ。", "やかましい。.wav", "せやな", "やあやあ諸君", "ｱｶﾈﾁｬﾝやで", "なんや？", "みんな大好きｱｶﾈﾁｬﾝやで", "zzz", "......"]
-          async with message.channel.typing():
-            await asyncio.sleep(0.75)
-            # メッセージを選ぶ確率を指定する
-            await message.channel.send(random.choices(say_list, k=1, weights=[0.80,1.40,0.50,1.20,1.10,0.70,1.50,0.70,0.30,0.10])[0])
+          await asyncio.sleep(0.75)
+          # メッセージを選ぶ確率を指定する
+          await message.channel.send(random.choices(say_list, k=1, weights=[0.80,1.40,0.50,1.20,1.10,0.70,1.50,0.70,0.30,0.10])[0])
 # botの動作に必要なトークンの記述(ここでは環境変数に登録したbotのトークンを呼び出して使用している)
 client.run(os.environ.get("DISCORD_BOT_TOKEN"))

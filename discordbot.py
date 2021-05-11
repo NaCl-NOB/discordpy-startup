7126faaee23be806e045981d1266059601e8e66f
NaCl-NOB <74619382+NaCl-NOB@users.noreply.github.com>
2021-05-11 10:34:37 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -86,14 +86,14 @@ async def on_message(message):
       
     # メンションで反応する
       
-    crab = "\N{U+1F980}"
+    #crab = "\N{U+1F980}"
 
     if message.content.startswith("asso"):
       await message.add_reaction(":pleading_face:")
     if message.content.startswith("コンギョ"):
       await message.add_reaction(":flag_kp: :rocket: :boom:")
     if message.content.startswith("かに"):
-      await Client.add_reaction(crab)
+      await Client.add_reaction(":crab:")
     if message.content.startswith("塩"):
       await message.add_reaction(":salt:")
     if message.content.startswith("ﾊﾞｹ"):
